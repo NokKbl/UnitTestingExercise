b9ee24430f1df2657062d9c803c28e1cd8700fcd
khaopanbit <khaopan1234@gmail.com>
2018-09-04 20:41:36 +0700

    Improve and test StringUtil

--- test/StringUtilTest.java
+++ test/StringUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 /**
  * Tests of the StringUtil methods.
+ * 
+ * @author Pittayoot R.R.Ratanakul
  */
 public class StringUtilTest {
 
@@ -14,9 +16,33 @@ public class StringUtilTest {
 
 	@Test
 	public void testIndexOf() {
-		String[] fruit = {"Apple", "Banana", "Grapes"};
+		String[] fruit = { "Apple", "Banana", "Grapes" };
 		assertEquals(0, StringUtil.indexOf("Apple", fruit));
 	}
 
-	//TODO write more tests. Test other cases.
+	@Test
+	public void testIndexOfString() {
+		String[] animal = { "Ant", "Bee", "Dog" };
+		assertEquals(1, StringUtil.indexOf("Bee", animal));
+	}
+
+	@Test
+	public void testIndexOfemptyString() {
+		String[] empty = {};
+		assertEquals(-1, StringUtil.indexOf("Omega lul", empty));
+
+		assertEquals(-1, StringUtil.indexOf("", empty));
+	}
+
+	@Test
+	public void testIndexOfwrongString() {
+		String[] honne = { "Day one", "I might", "You and Me", "Crying over you" };
+		assertEquals(-1, StringUtil.indexOf("Justin", honne));
+	}
+
+	@Test
+	public void testIndexOfsamevalue() {
+		String[] animal = { "Eagle", "Mice", "Mice", "Eagle" };
+		assertEquals(1, StringUtil.indexOf("Mice", animal));
+	}
 }
